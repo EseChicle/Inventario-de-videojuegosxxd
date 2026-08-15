38d3e03d3a9c37b1add542e99deb5c5a8b30bc9b
Luis Enrique López Portillo Dávila <compaluis123@gmail.com>
2026-08-14 22:56:06 -0600

    ventana principal y carga de productos

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import sqlite3
 import tkinter as tk
 from tkinter import ttk, messagebox
 
+#base de datos#
 DB_NAME = "inventario.db"
 def conectar_db():
     conn = sqlite3.connect(DB_NAME)
@@ -17,3 +18,22 @@ def conectar_db():
         """
     )
     return conn
+#ventana principal y carga de productos#
+
+class Inventario(tk.TK):
+    def __init__(self):
+        super().__init__()
+        self.title("Videojuegos Nevos")
+        self.geometry("1024x768")
+
+        style = ttk.Style(self)
+        style.configure(".",font=("Segoe UI", 12))
+        style.configure("Treeview", font=("Segoe UI", 11), rowheight=26)
+        style.configure("Treeview.Heading", font=("Segoe UI", 11, "bold"))
+
+        self.conn = conectar_db()
+        self.id_seleccionado = None
+
+        self._crear_widgets()
+        self.cargar_datos()
+        
